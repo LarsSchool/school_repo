247e7ab29c1f6bbf9a20a1c406ebb88d86b23277
Lars van Duijnhoven <LPW.vanDuijnhoven@student.han.nl>
2024-12-08 16:50:00 +0100

    Removed the limits for the stack, so I dont have to constantly change them when developing. I also found out that in order to make it repeat and not core dump after 1 try, I need to have it in an infinite loop, so thats what I did and then it worked. Furthermore I added an emergency stop that actually works and stops the measuring of the sensor.

--- Project-WoR/technische_leerdoel/Code/Lichtsensor_uitlezen_2/src/main.cpp
+++ Project-WoR/technische_leerdoel/Code/Lichtsensor_uitlezen_2/src/main.cpp
@@ -1,13 +1,15 @@
 /*
- * For more detail (instruction and wiring diagram), visit https://esp32io.com/tutorials/esp32-light-sensor
+ * For more detail (wiring diagram), visit https://esp32io.com/tutorials/esp32-light-sensor
  */
 
 #include <Arduino.h>
+#include <stdint.h>
 
 #define LIGHT_SENSOR_PIN 15 // For the ESP32 this is ADC pin 10
 #define LIGHT_MODES_AMOUNT 6
 
-TaskHandle_t lightsensorCheck;
+TaskHandle_t lightsensorCheckHandle;
+TaskHandle_t emergencyStopHandle;
 
 enum lightModes
 {
@@ -18,6 +20,26 @@ enum lightModes
   bright,
   veryBright
 };
+
+bool emergencyStopActive = false;
+
+void activateEmergencyStop(void *parameter)
+{
+
+  Serial.println("Emergency Stop activated!!");
+  emergencyStopActive = true;
+
+  vTaskDelete(NULL);
+}
+
+void dectivateEmergencyStop(void *parameter)
+{
+
+  Serial.println("Emergency Stop activated!!");
+  emergencyStopActive = true;
+
+  vTaskDelete(NULL);
+}
 
 void printLightSensorValue(unsigned short mappedValue)
 {
@@ -50,87 +72,49 @@ void printLightSensorValue(unsigned short mappedValue)
 
 void checkLightSensor(void *parameter)
 {
-  // Reads the input on analog pin (value between 0 and 4095)
-  // unsigned int analogValue = analogRead(LIGHT_SENSOR_PIN);
-  // unsigned short analogValueMapped = map(analogValue, 0, 4095, 0, LIGHT_MODES_AMOUNT - 1);
+  UBaseType_t stackHighWaterMark;
 
-  // Serial.print("Analog Value = ");
-  // Serial.print(analogValue); // The raw analog reading
-  // Serial.print("  -  Mapped Value = ");
-  // Serial.print(analogValueMapped); // The mapped analog value.
+  Serial.print("LightTask is running on core ");
+  Serial.println(xPortGetCoreID());
 
-  // printSensorValue(analogValueMapped);
+  for (;;)
+  {
+    if (!emergencyStopActive)
+    {
+      // Reads the input on analog pin (value between 0 and 4095)
+      uint16_t analogValue = analogRead(LIGHT_SENSOR_PIN);
+      uint8_t analogValueMapped = map(analogValue, 0, 4095, 0, LIGHT_MODES_AMOUNT - 1);
 
-  // Serial.println();
+      Serial.print("Analog Value = ");
+      Serial.print(analogValue); // The raw analog reading
+      Serial.print("  -  Mapped Value = ");
+      Serial.print(analogValueMapped); // The mapped analog value.
 
-  // vTaskDelay(500 / portTICK_PERIOD_MS);
-  // delay(500); // This can be removed after RTOS has been implemented and I can set a software timer there. I know a delay isn't all that nice.
+      printLightSensorValue(analogValueMapped);
+
+      Serial.println();
+
+      if (analogValue >= 4090)
+      {
+        xTaskCreatePinnedToCore(activateEmergencyStop, "activateEmergencyStop", 1024, NULL, 100, &emergencyStopHandle, 1);
+      }
+    }
+    else
+    {
+      Serial.println("EMERGENCY STOP ACTIVE, NOT READING SENSOR");
+    }
+
+    vTaskDelay(500 / portTICK_PERIOD_MS);
+  }
 }
 
 void setup()
 {
-  Serial.begin(9600);
+  Serial.begin(115200);
 
-  // pinMode(LIGHT_SENSOR_PIN, INPUT);
-
-  xTaskCreate(
-      checkLightSensor,   // Function that should be called
-      "checkLightSensor", // Name of the task (for debugging)
-      10000,              // Stack size (bytes), is a random estimate, this isnt based on anything.
-      NULL,               // Parameter to pass
-      1,                  // Task priority
-      &lightsensorCheck,  // Task handle
-      0                   // Selected core to run on
-  );
+  xTaskCreatePinnedToCore(checkLightSensor, "LightTask", 1024, NULL, 1, &lightsensorCheckHandle, 0);
 }
 
 void loop()
 {
-  // getAndPrintSensorValue();
 }
-
-// TaskHandle_t Task1;
-// TaskHandle_t Task2;
-
-// const int led_1 = 32;
-// const int led_2 = 25;
-
-// void Task1code( void * parameter ){
-//   Serial.print("Task1 is running on core ");
-//   Serial.println(xPortGetCoreID());
-
-//   for(;;){
-//     digitalWrite(led_1, HIGH);
-//     delay(500);
-//     digitalWrite(led_1, LOW);
-//     delay(500);
-//   }
-// }
-
-// void Task2code( void * parameter ){
-//   Serial.print("Task2 is running on core ");
-//   Serial.println(xPortGetCoreID());
-
-//   for(;;){
-//     digitalWrite(led_2, HIGH);
-//     delay(1000);
-//     digitalWrite(led_2, LOW);
-//     delay(1000);
-//   }
-// }
-
-// void setup() {
-//   Serial.begin(115200);
-//   pinMode(led_1, OUTPUT);
-//   pinMode(led_2, OUTPUT);
-
-//   xTaskCreatePinnedToCore(Task1code,"Task1",10000,NULL,1,&Task1,0);
-//   delay(500);
-
-//   xTaskCreatePinnedToCore(Task2code,"Task2",10000,NULL,1,&Task2,1);
-//     delay(500);
-// }
-
-// void loop() {
-
-// }
