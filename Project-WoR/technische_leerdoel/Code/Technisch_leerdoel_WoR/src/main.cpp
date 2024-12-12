8ac661f83cec9b9cdb762157a2dfb18a125f33ef
Lars van Duijnhoven <LPW.vanDuijnhoven@student.han.nl>
2024-12-12 12:59:52 +0100

    Made a new project to add the new temp sensor, so I would have added these in different projects and can refer to them individually. And as states above, I added the new temp sensor in this commit too! Its not perfect, but it works for most temperatures and gives a good indication.

--- Project-WoR/technische_leerdoel/Code/Technisch_leerdoel_WoR/src/main.cpp
+++ Project-WoR/technische_leerdoel/Code/Technisch_leerdoel_WoR/src/main.cpp
@@ -6,6 +6,7 @@
 #include <stdint.h>
 
 #define LIGHT_SENSOR_PIN 15 // For the ESP32 this is ADC pin 13
+#define TEMP_SENSOR_PIN 2   // For the ESP32 this is ADC pin 12
 #define LIGHT_MODES_AMOUNT 6
 
 enum lightModes
@@ -19,9 +20,12 @@ enum lightModes
 };
 
 TaskHandle_t lightsensorCheckHandle;
+TaskHandle_t tempsensorCheckHandle;
 TaskHandle_t emergencyStopHandle;
 
 bool emergencyStopActive = false;
+
+float tempC; // temperature in Celsius
 
 void activateEmergencyStop(void *parameter)
 {
@@ -79,8 +83,9 @@ void checkLightSensor(void *parameter)
     {
       // Reads the input on analog pin (value between 0 and 4095)
       uint16_t analogValue = analogRead(LIGHT_SENSOR_PIN);
+      uint8_t analogValueMapped = map(analogValue, 0, 4095, 0, LIGHT_MODES_AMOUNT - 1);
 
-      Serial.print("Analog Value = ");
+      Serial.print("[Light Sensor]: Analog Value = ");
       Serial.print(analogValue); // The raw analog reading
       Serial.print("  -  Mapped Value = ");
       Serial.print(analogValueMapped); // The mapped analog value.
@@ -103,11 +108,46 @@ void checkLightSensor(void *parameter)
   }
 }
 
+void checkTempSensor(void *parameter)
+{
+  Serial.print("TempTask is running on core ");
+  Serial.println(xPortGetCoreID());
+
+  for (;;)
+  {
+    if (!emergencyStopActive)
+    {
+      // Reads the input on analog pin (value between 0 and 4095)
+      uint16_t analogValue = analogRead(TEMP_SENSOR_PIN);
+
+      double tempC = map(analogValue, 0, 4095, -10, 125) - 4; // -4 has to be there so it can also go below 0.
+      
+      Serial.print("[Temperature Sensor]: Analog Value = ");
+      Serial.print(analogValue);
+      Serial.print("  -  Temperature: ");
+      Serial.print(tempC);
+      Serial.println("°C");
+
+      if (tempC >= 30.00 || tempC <= 10.00)
+      {
+        xTaskCreatePinnedToCore(activateEmergencyStop, "activateEmergencyStop", 1024, NULL, 100, &emergencyStopHandle, 1);
+      }
+    }
+    else
+    {
+      Serial.println("EMERGENCY STOP ACTIVE, NOT READING SENSOR");
+    }
+
+    vTaskDelay(500 / portTICK_PERIOD_MS);
+  }
+}
+
 void setup()
 {
-  Serial.begin(115200);
+  Serial.begin(9600);
 
   xTaskCreatePinnedToCore(checkLightSensor, "LightTask", 1024, NULL, 1, &lightsensorCheckHandle, 0);
+  xTaskCreatePinnedToCore(checkTempSensor, "TempTask", 1024, NULL, 1, &tempsensorCheckHandle, 1);
 }
 
 void loop()
